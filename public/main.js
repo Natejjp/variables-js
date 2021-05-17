2c0dfb4f9d16938b45d5158a252139e253bec8f8
Nathan Phame <diyf2021@gmail.com>
2021-05-17 13:25:50 -0400

    Added Object aboutMe

--- public/main.js
+++ public/main.js
@@ -2,6 +2,14 @@ function main() {
   const numberOfCups = 1
   const fullName = 'Nathan Pham'
   console.log(`${fullName} has ${numberOfCups} cup of coffee a day`)
+
+  const aboutMe = {
+    fullName: 'Nathan Pham',
+    favoriteNumber: 17,
+    favoriteMovies: ['Legally Blonde', 'Iron Man', 'Intersellar'],
+  }
+
+  console.log(aboutMe.fullName)
 }
 
 document.addEventListener('DOMContentLoaded', main)
